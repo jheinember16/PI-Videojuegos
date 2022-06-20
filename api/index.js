185be7fab255389eefbbd35bd768e9f2b0b14274
jheinember16 <jheinemberstithjn@ufps.edu.co>
2022-06-19 22:56:52 -0400

    Configurando index.js

--- api/index.js
+++ api/index.js
@@ -18,10 +18,29 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn, Genre } = require('./src/db.js');
+const axios = require("axios");
+const { API_KEY } = process.env;
+
+
+function preCharge() {
+  axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
+  .then((response) => {
+    let aux = response.data.results.map((ep) => {
+      const obj = {
+        id: ep.id,
+        name: ep.name,
+      }; 
+      return obj;
+    });
+    // Crear episodios en la base de datos
+    Genre.bulkCreate(aux);
+  });
+}
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  await preCharge()
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
