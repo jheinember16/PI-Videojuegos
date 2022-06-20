9b6867ae02d297ecb0343acd1b6049b0b03414bb
jheinember16 <jheinemberstithjn@ufps.edu.co>
2022-06-20 13:29:57 -0400

    Configurando app-index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LandingPage from "./components/LandingPage"
+import Home from './components/Home'
+import VideogameCreate from './components/VideogameCreate'
+import Details from './components/Details'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
-  );
-}
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/home" element={<Home />}></Route> 
+          <Route path= "/videogame" element ={<VideogameCreate/>}></Route>       
+          <Route path= '/details/:id' element={<Details/>}></Route> 
+        </Routes>
+    </BrowserRouter>
+  )}
 
 export default App;
